4019b586b89ce9b238189fbaad7510d76b9ac5e5
Jabuitroon <Jandresbuiitron@gmail.com>
2026-04-13 10:47:18 -0500

    Init Backend, config module, .env + zod validations

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { validate } from './config/env.load';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate,
+      isGlobal: true,
+      cache: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
